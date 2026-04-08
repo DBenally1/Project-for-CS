702976402cdb714d3bdadabe21c3705c4e8b4944
wescairo <rcurley2022@gmail.com>
2026-04-08 16:49:42 -0700

    Update split_assign.c

--- split_assign.c
+++ split_assign.c
@@ -1,34 +1,5 @@
 /*Filename: split_assign.c
-*Name: Reshawna Curley
-*Program: split array and assign to 2 hands
-*Version: 1.0 March 25, 2026
-*Resources:n/a
-*/
-#include <stdio.h>
-#define total_cards 52
-#define hand_size 26
-
-int main() {
-        int deck[total_cards];
-        int player1[hand_size];
-        int player2[hand_size];
-        int i;
-
-
-        /*fill the deck with values from 0-51*/
-
-        for (i=0; i < total_cards; i++) {
-                deck[i] = i;
-        }
-
-
-        /*Split*/
-
-        for (i=0; i < hand_size; i++) {
-                player1[i] = deck[i];
-                player2[i] = deck[i + hand_size];
-        }
-        /*Filename: split_assign.c
+/*Filename: split_assign.c
 *Name: Reshawna Curley
 *Program: split array and assign to 2 hands
 *Version: 1.0 March 25, 2026
