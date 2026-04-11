bcbfc4cc295bc1a2f413a8960e1dc31e1ee537ea
wescairo <rcurley2022@gmail.com>
2026-04-11 16:22:54 -0700

    Update split_assign.c
    added include split assign header and added some variables with the shuffle deck before split

--- split_assign.c
+++ split_assign.c
@@ -1,11 +1,13 @@
-/*Filename: split_assign.c
+
 /*Filename: split_assign.c
 *Name: Reshawna Curley
 *Program: split array and assign to 2 hands
 *Version: 1.0 March 25, 2026
-*Resources:n/a
+*Resources: lecture slides on void and indexing 
 */
 #include <stdio.h>
+#include <stdlib.h>
+#include "split_assign.h"
 #define total_cards 52
 #define hand_size 26
 
@@ -13,11 +15,19 @@ void split_assign(int player1[], int player2[])
 {
         int deck[total_cards];
         int i;
+        int j;
+        int temp;
         /*fill the deck with values from 0-51*/
         for (i=0; i < total_cards; i++) {
                 deck[i] = i;
         }
-
+        /*shuffles deck*/
+        for (i = total_cards - 1; i>10 ; i--) {
+                j = rand() % (i + 1);
+                temp = deck[i];
+                deck[i] = deck[j];
+                deck[j] = temp;
+                }
 
         /*Split and assign to 2 hands (player 1 and 2)*/
 
